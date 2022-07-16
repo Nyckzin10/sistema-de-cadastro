f8cabac50b6860d1fcffaa20cf3b4b039a4e13ac
Hedris <hedrisgts@gmail.com>
2022-07-16 02:17:19 -0300

    "sdsd"

--- project/settings.py
+++ project/settings.py
@@ -126,3 +126,5 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+
+LOGOUT_REDIRECT_URL = '/'
--- project/urls.py
+++ project/urls.py
@@ -4,6 +4,6 @@ from django.urls import path, include
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('home.urls')),
-    path('', include('core.urls')),
-    path('', include('django.contrib.auth.urls'))
+    path('pessoas/', include('core.urls')),
+    path('accounts/', include('django.contrib.auth.urls'))
 ]
